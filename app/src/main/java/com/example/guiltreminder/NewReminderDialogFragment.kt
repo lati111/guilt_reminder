ded842248f3c0afa7a0f4ab977c884fda567d067
lati111 <75079614+lati111@users.noreply.github.com>
2024-10-14 23:49:41 +0200

    new: added new reminder dialog

---
+++ app/src/main/java/com/example/guiltreminder/NewReminderDialogFragment.kt
@@ -0,0 +1,200 @@
+package com.example.guiltreminder
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.fragment.app.DialogFragment
+import java.lang.reflect.Type
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+
+class NewReminderDialogFragment(private val activity: OverviewActivity) : DialogFragment() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                var description by remember { mutableStateOf<String?>(null) }
+                var date by remember { mutableStateOf<Long?>(null) }
+                var time by remember { mutableStateOf<String?>(null) }
+
+                var showDescriptionInput by remember { mutableStateOf(true) }
+                var showDatepicker by remember { mutableStateOf(false) }
+                var showTimepicker by remember { mutableStateOf(false) }
+
+                @Composable
+                fun DescriptionField() {
+                    val textState = remember { mutableStateOf(TextFieldValue()) }
+                    val textFieldContentDescription = "Description"
+                    TextField(
+                        value = textState.value,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .semantics { contentDescription = textFieldContentDescription },
+                        onValueChange = {
+                            textState.value = it
+                            description = textState.value.text
+                        },
+                    )
+                }
+
+                @Composable
+                fun DescriptionDialog(
+                    onConfirmation: () -> Unit,
+                ) {
+                    AlertDialog(
+                        title = {
+                            Text(text = "Enter a description")
+                        },
+                        text = {
+                            DescriptionField()
+                        },
+                        onDismissRequest = {
+                            dismiss()
+                        },
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    onConfirmation()
+                                }
+                            ) {
+                                Text("Next")
+                            }
+                        },
+                        dismissButton = {
+                            TextButton(
+                                onClick = {
+                                    dismiss()
+                                }
+                            ) {
+                                Text("Cancel")
+                            }
+                        }
+                    )
+                }
+
+                @OptIn(ExperimentalMaterial3Api::class)
+                @Composable
+                fun DatePickerModal(
+                    onConfirm: () -> Unit,
+                    ) {
+                    val datePickerState = rememberDatePickerState()
+
+                    DatePickerDialog(
+                        onDismissRequest = {dismiss()},
+                        confirmButton = {
+                            TextButton(onClick = {
+                                date = datePickerState.selectedDateMillis
+                                onConfirm()
+//                                dismiss();
+                            }) {
+                                Text("Next")
+                            }
+                        },
+                        dismissButton = {
+                            TextButton(onClick = {dismiss()}) {
+                                Text("Cancel")
+                            }
+                        }
+                    ) {
+                        DatePicker(state = datePickerState)
+                    }
+                }
+
+                @Composable
+                fun TimePickerDialog(
+                    onConfirm: () -> Unit,
+                ) {
+                    val currentTime = Calendar.getInstance()
+
+                    val timePickerState = rememberTimePickerState(
+                        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+                        initialMinute = currentTime.get(Calendar.MINUTE),
+                        is24Hour = true,
+                    )
+
+                    Column {
+                        TimePicker(
+                            state = timePickerState,
+                        )
+                        Button(onClick = {dismiss()}) {
+                            Text("Cancel")
+                        }
+                        Button(onClick = {
+                            time = timePickerState.hour.toString() + ":" + timePickerState.minute.toString()
+                            onConfirm()
+                        }) {
+                            Text("Confirm")
+                        }
+                    }
+                }
+
+                if (showDescriptionInput) {
+                    DescriptionDialog(
+                        onConfirmation = {
+                            showDescriptionInput = false;
+                            showDatepicker = true;
+                        },
+                    )
+                }
+
+                if (showDatepicker) {
+                    DatePickerModal(
+                        onConfirm = {
+                            showDatepicker = false;
+                            showTimepicker = true
+                        },
+                    )
+                }
+
+                if (showTimepicker) {
+                    TimePickerDialog(
+                        onConfirm = {
+                            var timestamp: String = "";
+                            if (date !== null) {
+                                timestamp = SimpleDateFormat("yyyy.MM.dd").format(Date(date!!));
+                            }
+
+                            timestamp += " $time";
+
+                            activity.addReminder(description!!, timestamp)
+                            dismiss();
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/guiltreminder/OverviewActivity.kt
+++ app/src/main/java/com/example/guiltreminder/OverviewActivity.kt
@@ -3,19 +3,46 @@ package com.example.guiltreminder
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.Pager
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.reflect.Modifier
 import java.util.Date
 
 class OverviewActivity : AppCompatActivity() {
     private lateinit var mReminderRecyclerView: RecyclerView
     private lateinit var mReminderAdapter: ReminderAdapter
+    private val myLive = MutableLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_overview)
 
@@ -39,5 +66,16 @@ class OverviewActivity : AppCompatActivity() {
 
         mReminderAdapter.setmAllReminders(reminders);
 
+        val newReminderButton: ImageButton = findViewById(R.id.newReminderButton)
+        newReminderButton.setOnClickListener(View.OnClickListener { view: View ->
+            val newFragment = NewReminderDialogFragment(this)
+            newFragment.show(supportFragmentManager, "game")
+        })
     }
+
+    public fun addReminder(description: String, timestamp: String) {
+        Log.i("description", description);
+        Log.i("timestamp", timestamp);
+    }
+
 }
